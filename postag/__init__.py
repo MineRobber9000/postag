8d5b5f7209ee5f838c2a31e130843d1e1c105d06
khuxkm fbexl <khuxkm@ttm.sh>
2019-03-16 00:44:33 +0000

    Add singularize and default-to-noun filters

--- postag/__init__.py
+++ postag/__init__.py
@@ -9,6 +9,23 @@ def default_resolver(tag):
 	return True,tag
 tag_resolver.addResolver(default_resolver,-1000)
 
+from nltk.stem.snowball import SnowballStemmer
+stemmer = SnowballStemmer("english")
+
+def singularize(w): return stemmer.stem(w)
+
+def singularize_plurals(tag):
+	if tag[1] is not None: return False, None
+	w = singularize(tag[0])
+	if postag.tagger.tag([w])[0][1] is None: return False, None
+	return True, (tag[0],postag.tagger.tag([w])[0][1])
+tag_resolver.addResolver(singularize_plurals,0)
+
+def tag_as_noun(tag):
+	if tag[1] is not None: return False, None
+	return True, (tag[0],"NOUN")
+tag_resolver.addResolver(tag_as_noun,-999)
+
 tagger = UnigramTagger(treebank.tagged_sents(tagset="universal"))
 
 def tag(sent):
